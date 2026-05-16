09ab9b9fdb89b6f8b78162c082dae2770ae3a3c3
Debayo Yakubu <debayoyakubu@Debayos-MacBook-Pro.local>
2026-05-16 12:55:11 +0100

    fix: update EKS version to 1.32

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 resource "aws_eks_cluster" "main" {
   name     = "${var.project_name}-cluster"
   role_arn = aws_iam_role.eks_cluster.arn
-  version  = "1.29"
+  version  = "1.32"
 
   vpc_config {
     subnet_ids = var.private_subnet_ids
